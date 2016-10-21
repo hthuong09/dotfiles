e83148ece1928d39dac6e7d3a116061be61b78ba
Thuong Nguyen <thuongnguyen.net@gmail.com>
2016-10-21 09:47:32 +0700

    Add PATH for global npm, yarn

--- .zsh/alias/environment.zsh
+++ .zsh/alias/environment.zsh
@@ -1,19 +1,15 @@
-if [[ -d "$HOME/bin" ]] ; then
-    PATH="$HOME/bin:$PATH"
-fi
-
 if [[ -d "$HOME/.config/composer/vendor/bin" ]]; then
     PATH="$HOME/.config/composer/vendor/bin:$PATH"
 fi
 
 # Go language
-if which go &>/dev/null; then
+if hash go 2>/dev/null; then
     export GOPATH=~/go
     export PATH=$PATH:~/go/bin
 fi
 
 # Google Chrome
-if which google-chrome-stable &>/dev/null; then
+if hash google-chrome-stable 2>/dev/null; then
     export CHROME_BIN=/usr/bin/google-chrome-stable
 fi
 
@@ -23,9 +19,25 @@ if [[ -d "/opt/android-sdk" ]]; then
 fi
 
 # Editor
-if which nvim &>/dev/null; then
+if hash nvim 2>/dev/null; then
     alias vim=nvim
     export EDITOR="vim"
 else
     export EDITOR="nano"
 fi
+
+# npm global without root
+if hash npm 2>/dev/null; then
+    if [ ! -d "$HOME/.npm-global" ]; then
+        mkdir ~/.npm-global
+    fi
+
+    NPM_CONFIG_PREFIX=~/.npm-global
+    export PATH=~/.npm-global/bin:$PATH
+fi
+
+# Yarn package manager
+if hash yarn 2>/dev/null; then
+    export PATH=~/.yarn/bin:$PATH
+fi
+
