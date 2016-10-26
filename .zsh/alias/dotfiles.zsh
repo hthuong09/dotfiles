a5b1753fa178cc1127ba9b5ccef72c3ae80c34db
Thuong Nguyen <thuongnguyen.net@gmail.com>
2016-10-26 22:29:29 +0700

    Update git alias, ncmpcpp config

--- .zsh/alias/dotfiles.zsh
+++ .zsh/alias/dotfiles.zsh
@@ -1,3 +1,2 @@
 alias dots='git --git-dir=$HOME/.gdots/ --work-tree=$HOME'
-alias dots-add-modified='dots ls-files --modified | xargs git --git-dir=$HOME/.gdots/ --work-tree=$HOME add'
 dots config status.showUntrackedFiles no
