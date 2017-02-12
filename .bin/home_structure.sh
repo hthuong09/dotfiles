ff1a69264c5264f378451c3a0e4259c1c33c76ca
Thuong Nguyen <thuongnguyen.net@gmail.com>
2017-02-12 22:46:59 +0700

    Add home folder structure file

---
+++ .bin/home_structure.sh
@@ -0,0 +1,32 @@
+#!/bin/env bash
+: <<'explain'
+
+bin             # runnable binary file ($PATH)
+code            # code, projects, things
+docs            # documents, tutorials, references
+etc             # misc, extra things
+    conf        # any other configure file
+    vm          # virtual machines file
+media           # multimedia
+    musics      # global music in system
+    pictures    # pics
+    videos      # video, pỏn
+net             # things from internet
+    downloads   # downloaded file from internet
+    torrents    # torrent from internet, mostly temporary
+storage         # storage, contains links to external storage, fuse
+    acd         # Amazon Cloud Drive
+    data        # HDD
+    gd          # Google Drive
+tmp             # temporary files
+weeb            # Weeboo things
+    amv         # MAD, AMV, Anime Music Video
+    anime       # anime
+        movie   # OVA, Movie
+        series  # TV series
+    anisong     # Song from anime, j-pop
+    manga       # manga
+    
+explain
+
+mkdir -p ~/{bin,code,docs,etc/{conf,vm},media/{musics,pictures,videos},net/{downloads,torrents},storage/{acd,data,gd},weeb/{amv,anime/{movie,series},anisong,manga},tmp}
