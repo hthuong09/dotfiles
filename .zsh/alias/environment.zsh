1428be7f0219b6eec44943975bd53005b7d93519
Thuong Nguyen <thuongnguyen.net@gmail.com>
2017-12-07 21:19:49 +0700

    Change method checking for yarn prefix for better performance

--- .zsh/alias/environment.zsh
+++ .zsh/alias/environment.zsh
@@ -41,6 +41,12 @@ node_packages_preparing() {
 
 }
 
+get_yarn_prefix_path() {
+  yarnrc="$HOME/.yarnrc"
+  
+  echo $(cat $yarnrc | grep -oP '(?<=prefix ").*(?=")')
+}
+
 # npm global without root
 if hash npm 2>/dev/null; then
     node_packages_preparing $NODE_PACKAGES_PATH
@@ -48,11 +54,11 @@ if hash npm 2>/dev/null; then
     export NPM_CONFIG_PREFIX=$NODE_PACKAGES_PATH
 fi
 
-# Yarn package manager
+# arn package manager
 if hash yarn 2>/dev/null; then
   node_packages_preparing $NODE_PACKAGES_PATH
 
-  if [[ $(yarn config get prefix) != $NODE_PACKAGES_PATH ]]; then
+  if [[ $(get_yarn_prefix_path) != $NODE_PACKAGES_PATH ]]; then
     yarn config set prefix $NODE_PACKAGES_PATH > /dev/null
   fi
 fi
